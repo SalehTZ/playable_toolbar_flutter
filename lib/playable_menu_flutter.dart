c0c2559671cc28732b12974e8d84ee9d5535de9d
salehtz <m.s_ta@live.com>
2022-07-10 11:28:17 +0430

    first commit

--- lib/playable_menu_flutter.dart
+++ lib/playable_menu_flutter.dart
@@ -132,24 +132,18 @@ class _PlayableMenuWidgetState extends State<PlayableMenuWidget> {
                 isLongPressed = false;
                 _updateLongPressedItemsFlags(longPressYLocation: 0);
               },
-              child: AnimatedContainer(
-                duration: Constants.longPressAnimationDuration,
-                width: isLongPressed
-                    ? Constants.toolbarWidth * 3
-                    : Constants.toolbarWidth,
-                child: ListView.builder(
-                  controller: scrollController,
-                  itemCount: widget.toolbarItems.length,
-                  padding: const EdgeInsets.all(10),
-                  itemBuilder: (context, index) {
-                    return SideBarItem(
-                      widget.toolbarItems[index],
-                      height: itemHeight,
-                      scrollScale: itemScrollScaleValues[index],
-                      isLongPressed: longPressedItemsFlags[index],
-                    );
-                  },
-                ),
+              child: ListView.builder(
+                controller: scrollController,
+                itemCount: widget.toolbarItems.length,
+                padding: const EdgeInsets.all(10),
+                itemBuilder: (context, index) {
+                  return SideBarItem(
+                    widget.toolbarItems[index],
+                    height: itemHeight,
+                    scrollScale: itemScrollScaleValues[index],
+                    isLongPressed: longPressedItemsFlags[index],
+                  );
+                },
               ),
             )
           ],
